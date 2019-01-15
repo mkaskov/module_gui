c0200a62071e5dc446c62025a4d320bcd6ac3e53
Andrew <a.savel.vic@gmail.com>
2019-01-15 20:20:40 +0400

    upd install.bash

--- puremindgui/script/install.bash
+++ puremindgui/script/install.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 sudo apt install libappstream3 -y
 sudo apt install git -y
-sudo apt install python-pip -y
+sudo apt install python3-pip -y
 sudo apt install nmap -y
-sudo pip2 install pyinstaller
+sudo pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04 wxPython
+sudo pip3 install pyinstaller
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
@@ -13,7 +14,6 @@ rosdep update
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-echo "ROS_IP=$(hostname -I | cut -d' ' -f1)" >> ~/.bashrc
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src/
 git clone https://github.com/mkaskov/module_gui
